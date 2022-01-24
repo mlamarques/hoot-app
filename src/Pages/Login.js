38f12e72d5eff8b53d8c5ae1c22e91453ea5ca74
Mark Alves <lamarques.marcos@gmail.com>
2022-01-24 10:50:59 -0300

    Create initial boilerplate

---
+++ src/Pages/Login.js
@@ -0,0 +1,12 @@
+import Logo from '../assets/Logo'
+
+function Login() {
+    return (
+      <div className="LoginPage">
+        <h1>Log in</h1>
+        <Logo />
+      </div>
+    );
+  }
+  
+  export default Login;
