8cad3f37a23095f0088070ba4a3773a932e3a150
Mark Alves <lamarques.marcos@gmail.com>
2022-02-04 23:45:54 -0300

    Create user card component

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -2,10 +2,12 @@ import {UserCardStyle} from './styles'
 
 export default function UserCard(props) {
     return (
-      <UserCardStyle> 
+      
         <div className="usercard__component" onClick={props.handleClick}>
-          <h3>{props.username}</h3>
+          <UserCardStyle> 
+            <h3>{props.username}</h3>
+          </UserCardStyle>
         </div>
-      </UserCardStyle>
+      
     );
   }
