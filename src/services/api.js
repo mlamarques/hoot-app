06db3aaefac1239145aaa5f14bd1f839ebb74399
Mark Alves <lamarques.marcos@gmail.com>
2022-02-08 14:33:11 -0300

    Fix: Header authorization not updating

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,15 @@ import axios from 'axios'
 
 export const api = axios.create({
     baseURL: 'http://localhost:5432',
+    timeout: 10000,
     headers: {
-        'Content-Type': 'application/json'
-        // 'Authorization': `Bearer ${localStorage.getItem('@tokenG5T2Afya')}`
-    }
+        'Content-Type': 'application/json',
+        // 'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+    },
+    transformRequest: [(data, headers) => {
+        // You may modify the headers object here
+        headers['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`
+        // Do not change data
+        return data;
+      }, ...axios.defaults.transformRequest]
 })
