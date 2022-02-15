da20ab4f92fdb6cf66581f2288b8be15ca6c8bf5
Mark Alves <lamarques.marcos@gmail.com>
2022-02-15 16:30:42 -0300

    Feature: create search user

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,12 +1,36 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import {SearchStyle} from './styles'
 import Nav from '../../components/Nav/Nav'
 import { UserContext } from '../../context/UserContext'
+import { api } from '../../services/api'
 
 
 export default function Search(props) {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [autoComplete, setAutoComplete] = useState([])  
 
   const { user } = useContext(UserContext)
+
+  useEffect( () => {
+    if (searchTerm.length) {
+      try {
+        api.post('/user/search', { "term": searchTerm })
+          .then(res => {
+            console.log(res.data)
+            setAutoComplete(res.data.list_users)
+          })
+      } catch (error) {
+        console.error(error)
+      }
+    } else {
+      setAutoComplete([])
+    }
+  }, [searchTerm])
+
+  function handleChange(event) {
+    const { value } = event.target
+    setSearchTerm(value)
+  }
 
   return (
     <div className="search-page">
@@ -14,11 +38,27 @@ export default function Search(props) {
       <SearchStyle>
       <div className="main__container">
         <div className="main__header">
-          <h1 onClick={props.handleLogout} >Search</h1>
+          <h1>Search</h1>
         </div>
         <div className="feed__container">
           <div className="hoot--individual__container">
             <h2>Search here...</h2>
+            <input type="text" value={searchTerm} onChange={handleChange}/>
+            {autoComplete.length > 0 && (
+              <ul className="user-card_container">
+                {autoComplete.map(item => {
+                  return (
+                    <li
+                      key={item?._id}
+                      className="user-card"
+                      onClick={() => console.log(item?._id)}
+                    >
+                      {item?.username}
+                    </li>
+                  )
+                })}
+              </ul>
+            )}
           </div>
         </div>
       </div>
